e32c93f923d039e9f384f1af48d851382ed1df75
AnuchitO <anuchit.prasertasng@gmail.com>
2022-02-19 15:45:34 +0700

    split loop

--- prints-bill/bill.go
+++ prints-bill/bill.go
@@ -66,18 +66,20 @@ func volumeCreditsFor(plays Plays, perf Performance) (credits float64) {
 
 func statement(invoice Invoice, plays Plays) string {
 	totalAmount := 0.0
-	volumeCredits := 0.0
-	result := fmt.Sprintf("Statement for %s\n", invoice.Customer)
-
 	for _, perf := range invoice.Performances {
 		totalAmount += amountFor(plays, perf)
+	}
 
-		// add volume credits
+	volumeCredits := 0.0
+	for _, perf := range invoice.Performances {
 		volumeCredits += volumeCreditsFor(plays, perf)
+	}
 
-		// print line for this order
+	result := fmt.Sprintf("Statement for %s\n", invoice.Customer)
+	for _, perf := range invoice.Performances {
 		result += fmt.Sprintf("  %s: $%.2f (%d seats)\n", playName(playFor(plays, perf)), amountFor(plays, perf)/100, perf.Audience)
 	}
+
 	result += fmt.Sprintf("Amount owed is $%.2f\n", totalAmount/100)
 	result += fmt.Sprintf("you earned %.0f credits\n", volumeCredits)
 	return result

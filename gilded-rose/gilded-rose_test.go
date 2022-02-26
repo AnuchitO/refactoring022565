ac3886912cc5caffd9d7b9897d64d8a4e6980926
AnuchitO <anuchit.prasertasng@gmail.com>
2022-02-26 14:47:44 +0700

    add test coverage

--- gilded-rose/gilded-rose_test.go
+++ gilded-rose/gilded-rose_test.go
@@ -6,31 +6,113 @@ import (
 	"testing"
 )
 
-func Test_Foo(t *testing.T) {
-	cases := []struct {
-		name    string
-		inputs  []*Item
-		outputs []*Item
-	}{
-		{name: "#1", inputs: []*Item{{name: "+5 Dexterity Vest", sellIn: 10, quality: 20}}, outputs: []*Item{{name: "+5 Dexterity Vest", sellIn: 9, quality: 19}}},
-		{name: "#2", inputs: []*Item{{name: "Aged Brie", sellIn: 2, quality: 0}}, outputs: []*Item{{name: "Aged Brie", sellIn: 1, quality: 1}}},
-		{name: "#3", inputs: []*Item{{name: "Elixir of the Mongoose", sellIn: 5, quality: 7}}, outputs: []*Item{{name: "Elixir of the Mongoose", sellIn: 4, quality: 6}}},
-		{name: "#4", inputs: []*Item{{name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80}}, outputs: []*Item{{name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80}}},
-		{name: "#5", inputs: []*Item{{name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80}}, outputs: []*Item{{name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80}}},
-		{name: "#6", inputs: []*Item{{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 15, quality: 1}}, outputs: []*Item{{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 14, quality: 2}}},
-		{name: "#7", inputs: []*Item{{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 49}}, outputs: []*Item{{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 9, quality: 50}}},
-		{name: "#8", inputs: []*Item{{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 49}}, outputs: []*Item{{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 4, quality: 50}}},
-		{name: "#9", inputs: []*Item{{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 48}}, outputs: []*Item{{name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 9, quality: 50}}},
-	}
+type Cases struct {
+	name    string
+	inputs  []*Item
+	outputs []*Item
+}
 
-	for _, c := range cases {
-		t.Run(c.name, func(t *testing.T) {
-			UpdateQuality(c.inputs)
+func TestUpdateQuality(t *testing.T) {
+	t.Run("fixture test cases", func(t *testing.T) {
+		cases := []Cases{
+			{name: "#1", inputs: []*Item{{"+5 Dexterity Vest", 10, 20}}, outputs: []*Item{{"+5 Dexterity Vest", 10 - 1, 20 - 1}}},
+			{name: "#2", inputs: []*Item{{"Aged Brie", 2, 0}}, outputs: []*Item{{"Aged Brie", 2 - 1, 1}}},
+			{name: "#3", inputs: []*Item{{"Elixir of the Mongoose", 5, 7}}, outputs: []*Item{{"Elixir of the Mongoose", 5 - 1, 6}}},
+			{name: "#4", inputs: []*Item{{"Sulfuras, Hand of Ragnaros", 0, 80}}, outputs: []*Item{{"Sulfuras, Hand of Ragnaros", 0, 80}}},
+			{name: "#5", inputs: []*Item{{"Sulfuras, Hand of Ragnaros", -1, 80}}, outputs: []*Item{{"Sulfuras, Hand of Ragnaros", -1, 80}}},
+			{name: "#6", inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 15, 1}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 15 - 1, 2}}},
+			{name: "#7", inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 10, 49}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 10 - 1, 50}}},
+			{name: "#8", inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 5, 49}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 5 - 1, 50}}},
+			//{name: "#1", inputs: []*Item{{"Conjured Mana Cake", 3, 6}},  outputs: []*Item{{"Conjured Mana Cake", 3-1, 6-2}}}, // TODO: implement new requirement
+		}
 
-			if !reflect.DeepEqual(c.inputs, c.outputs) {
-				pretty.Println(c.inputs, c.outputs)
-				t.Errorf("Name: Expected %#v but got %#v ", c.outputs, c.inputs)
-			}
-		})
+		assert(t, cases)
+	})
+
+	t.Run("normal items", func(t *testing.T) {
+		cases := []Cases{
+			{name: "#1", inputs: []*Item{{"+5 Dexterity Vest", 5, 15}}, outputs: []*Item{{"+5 Dexterity Vest", 4, 14}}},
+			{name: "#2", inputs: []*Item{{"+5 Dexterity Vest", 4, 14}}, outputs: []*Item{{"+5 Dexterity Vest", 3, 13}}},
+			{name: "#3", inputs: []*Item{{"+5 Dexterity Vest", 3, 13}}, outputs: []*Item{{"+5 Dexterity Vest", 2, 12}}},
+			{name: "#4", inputs: []*Item{{"+5 Dexterity Vest", 2, 12}}, outputs: []*Item{{"+5 Dexterity Vest", 1, 11}}},
+			{name: "#5", inputs: []*Item{{"+5 Dexterity Vest", 1, 11}}, outputs: []*Item{{"+5 Dexterity Vest", 0, 10}}},
+			{name: "#6", inputs: []*Item{{"+5 Dexterity Vest", 0, 10}}, outputs: []*Item{{"+5 Dexterity Vest", -1, 8}}},
+			{name: "#7", inputs: []*Item{{"+5 Dexterity Vest", -1, 8}}, outputs: []*Item{{"+5 Dexterity Vest", -2, 6}}},
+			{name: "#8", inputs: []*Item{{"+5 Dexterity Vest", -2, 6}}, outputs: []*Item{{"+5 Dexterity Vest", -3, 4}}},
+			{name: "#9", inputs: []*Item{{"+5 Dexterity Vest", -3, 4}}, outputs: []*Item{{"+5 Dexterity Vest", -4, 2}}},
+			{name: "#10", inputs: []*Item{{"+5 Dexterity Vest", -4, 2}}, outputs: []*Item{{"+5 Dexterity Vest", -5, 0}}},
+			{name: "#11", inputs: []*Item{{"+5 Dexterity Vest", -5, 0}}, outputs: []*Item{{"+5 Dexterity Vest", -6, 0}}},
+
+			{name: "#12", inputs: []*Item{{"Elixir of the Mongoose", 1, 3}}, outputs: []*Item{{"Elixir of the Mongoose", 0, 2}}},
+			{name: "#13", inputs: []*Item{{"Elixir of the Mongoose", 0, 2}}, outputs: []*Item{{"Elixir of the Mongoose", -1, 0}}},
+			{name: "#14", inputs: []*Item{{"Elixir of the Mongoose", -1, 0}}, outputs: []*Item{{"Elixir of the Mongoose", -2, 0}}},
+			{name: "#15", inputs: []*Item{{"Elixir of the Mongoose", -2, 0}}, outputs: []*Item{{"Elixir of the Mongoose", -3, 0}}},
+		}
+
+		assert(t, cases)
+	})
+
+	t.Run("Aged Brie items", func(t *testing.T) {
+
+		cases := []Cases{
+			{name: "#1", inputs: []*Item{{"Aged Brie", 2, 0}}, outputs: []*Item{{"Aged Brie", 1, 1}}},
+			{name: "#2", inputs: []*Item{{"Aged Brie", 1, 1}}, outputs: []*Item{{"Aged Brie", 0, 2}}},
+			{name: "#3", inputs: []*Item{{"Aged Brie", 0, 2}}, outputs: []*Item{{"Aged Brie", -1, 4}}},
+			{name: "#4", inputs: []*Item{{"Aged Brie", -1, 4}}, outputs: []*Item{{"Aged Brie", -2, 6}}},
+			{name: "#5", inputs: []*Item{{"Aged Brie", -2, 6}}, outputs: []*Item{{"Aged Brie", -3, 8}}},
+			{name: "#6", inputs: []*Item{{"Aged Brie", -3, 8}}, outputs: []*Item{{"Aged Brie", -4, 10}}},
+		}
+
+		assert(t, cases)
+	})
+
+	t.Run("Sulfuras, Hand of Ragnaros items", func(t *testing.T) {
+
+		cases := []Cases{
+			{name: "#1", inputs: []*Item{{"Sulfuras, Hand of Ragnaros", 0, 80}}, outputs: []*Item{{"Sulfuras, Hand of Ragnaros", 0, 80}}},
+			{name: "#2", inputs: []*Item{{"Sulfuras, Hand of Ragnaros", -1, 80}}, outputs: []*Item{{"Sulfuras, Hand of Ragnaros", -1, 80}}},
+		}
+
+		assert(t, cases)
+	})
+
+	t.Run("Backstage passes to a TAFKAL80ETC concert items", func(t *testing.T) {
+
+		cases := []Cases{
+			{name: "#1", inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 12, 23}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 11, 24}}},
+			{name: "#2", inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 11, 24}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 10, 25}}},
+			{name: "#3", inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 10, 25}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 9, 27}}},
+			{name: "#4", inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 9, 27}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 8, 29}}},
+			{name: "#5", inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 8, 29}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 7, 31}}},
+			{name: "#6", inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 7, 31}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 6, 33}}},
+			{name: "#7", inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 6, 33}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 5, 35}}},
+			{name: "#8", inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 5, 35}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 4, 38}}},
+			{name: "#9", inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 4, 38}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 3, 41}}},
+			{name: "#10", inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 3, 41}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 2, 44}}},
+			{name: "#11", inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 2, 44}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 1, 47}}},
+			{name: "#12", inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 1, 47}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 0, 50}}},
+			{name: "#13", inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", 0, 50}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", -1, 0}}},
+			{name: "#14", inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", -1, 0}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", -2, 0}}},
+			{name: "#15", inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", -2, 0}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", -3, 0}}},
+			{name: "#16", inputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", -3, 0}}, outputs: []*Item{{"Backstage passes to a TAFKAL80ETC concert", -4, 0}}},
+		}
+
+		assert(t, cases)
+	})
+
+	t.Run("Conjured items", func(t *testing.T) {
+		t.Skip("TODO: implement new requirements")
+	})
+
+}
+
+func assert(t *testing.T, cases []Cases) {
+	for i, c := range cases {
+
+		UpdateQuality(c.inputs)
+
+		if !reflect.DeepEqual(c.inputs, c.outputs) {
+			t.Errorf("test %s nubmer #%d not match : \n%#v", t.Name(), i, pretty.Diff(c.inputs, c.outputs))
+		}
 	}
 }
